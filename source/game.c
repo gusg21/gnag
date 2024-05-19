1acb6402a6b5ddbbc751ea67f7e5d4b3dbe01448
James (Laptop) <james.mckibbin@mymail.champlain.edu>
2024-05-19 19:01:29 -0400

    aligned and sped up test move

--- source/game.c
+++ source/game.c
@@ -51,11 +51,11 @@ void Game_Update(game_t* game, float delta_secs) {
             character_t* current = Board_GetCurrentActingCharacter(&game->board);
             Board_EnqueuePlayerControlledCharacterAction(&game->board,
                         (character_action_t){.character = current,
-                                             .duration = 1.f,
+                                             .duration = 0.5f,
                                              .initialized = true,
                                              .type = ACTION_MOVE,
                                              .move_source = current->pos,
-                                             .move_destination = Vec2_Add(current->pos, (vec2_t){50.f, 0.f})});
+                                             .move_destination = Vec2_Add(current->pos, (vec2_t){32.f, -16.f})});
             CTR_PRINTF("tile selected\n");
             Game_UpdateGameState(game, GAME_STATE_PLAYER_TURN);
         }
