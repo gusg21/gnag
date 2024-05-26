c1d89673a95ce52500a0e1095d13ce1e4da21cae
[PC] Angus Goucher <gus.goucher@gmail.com>
2024-05-26 03:14:55 -0400

    added library installer helper script + DSP firmware

---
+++ installlibraries.bat
@@ -0,0 +1,13 @@
+@echo off
+
+pacman -S 3ds-libogg 3ds-libopus 3ds-opusfile --noconfirm
+
+mkdir "%APPDATA%\Citra\sdmc\3ds" 2>nul
+
+cp misc/dspfirm.cdc "%APPDATA%\Citra\sdmc\3ds"
+
+echo.
+echo [92mUpdated/installed libraries and copied 3DS sound firmware to Citra![0m
+echo.
+
+pause
