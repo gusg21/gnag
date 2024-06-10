cb82d6ddcbb1a5b2d397f585b5c5392d628c6cc2
James <james.mckibbin@mymail.champlain.edu>
2024-06-10 01:48:46 -0400

    hazard table filled

--- common/hazarddata.h
+++ common/hazarddata.h
@@ -2,9 +2,26 @@
 #define HAZARDDATA_H
 
 typedef enum {
+    // None 0
     HAZARD_NONE,
+    // Spikes 1-4
     HAZARD_SPIKES,
+    HAZARD_FLAMING_SPIKES,
+    HAZARD_FROZEN_SPIKES,
+    HAZARD_ELECTRIC_SPIKES,
+    // Water 5-7
     HAZARD_WATER,
+    HAZARD_STEAM,
+    HAZARD_ELECTRIC_WATER,
+    // Fire 8
+    HAZARD_FIRE,
+    // Ice 9
+    HAZARD_ICE,
+    // Lightning 10
+    HAZARD_LIGHTNING,
+    // Wind 11
+    HAZARD_GUST,
+
     HAZARD_COUNT
 } hazard_type_e;
 
